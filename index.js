a6b0273bad641dae522fc0b19162cf974251d74a
Walter Simon <simonwalter986@gmail.com>
2022-06-16 11:40:19 +0300

    commit tree 47de0f536076433039d785173384da1fc965d2d6

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let ride;
+  
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride > 2000 < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+let city = (city = 'NYC') ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip) {
+    case  generous:
+         "Thank you so much."
+      break;
+    case notAsGenerous:
+          "Thank you.";
+    default: "Bye"
+      break;
+  }
 }
